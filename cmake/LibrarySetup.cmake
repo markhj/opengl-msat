7a9c30132faf2dc03a78e361e6028cce6efa5b5e
markhj <hello@markhj.net>
2023-09-06 13:35:47 +0200

    Add Bindable interface class

--- cmake/LibrarySetup.cmake
+++ cmake/LibrarySetup.cmake
@@ -1,11 +1,25 @@
+# The list of classes in the library
 set(CLASSES timer/timer)
 
+# The list of contracts in the library
+set(CONTRACTS bindable)
+
+# Declare the variable which will contain the parsed lists
+# of sources and headers
 set(OPENGL_MSAT_HEADER_FILES "")
 set(OPENGL_MSAT_SOURCE_FILES "")
 
+# Iterate over the classes and append their header and source files to
+# the respective variables
 foreach(CLASS ${CLASSES})
     list(APPEND OPENGL_MSAT_HEADER_FILES "${PACKAGE_PREFIX_DIR}/include/opengl_msat/${CLASS}.hpp")
     list(APPEND OPENGL_MSAT_SOURCE_FILES "${PACKAGE_PREFIX_DIR}/src/opengl_msat/${CLASS}.cpp")
 endforeach()
 
+# Repeat the same for contracts (they only have header files)
+foreach (CONTRACT ${CONTRACTS})
+    list(APPEND OPENGL_MSAT_HEADER_FILES "${PACKAGE_PREFIX_DIR}/include/opengl_msat/contracts/${CONTRACT}.hpp")
+endforeach ()
+
+# Add the library with the source and header files
 add_library(opengl_msat ${OPENGL_MSAT_HEADER_FILES} ${OPENGL_MSAT_SOURCE_FILES})
--- include/opengl_msat/timer/timer.hpp
+++ include/opengl_msat/timer/timer.hpp
@@ -9,9 +9,9 @@ public:
 
     void end();
 
-    double getDeltaTime() const;
+    [[nodiscard]] double getDeltaTime() const;
 
-    unsigned int getFramesPerSecond() const;
+    [[nodiscard]] unsigned int getFramesPerSecond() const;
 private:
     double deltaTime = 0.0f;
     std::chrono::time_point<std::chrono::high_resolution_clock> startTime;
