eda973eef401807ee95b12b28779cca371d04266
mhj-pc <5261387+markhj@users.noreply.github.com>
2023-09-11 16:31:41 +0200

    2D position support in vertex builder

--- include/opengl_msat/shader/fragment_sbldr.hpp
+++ include/opengl_msat/shader/fragment_sbldr.hpp
@@ -19,6 +19,8 @@ public:
         std::string lines = "#version 330 core\n";
         lines += "out vec4 result;\n";
 
+        bool hasColor = false;
+
         for (VertexAttribute attr : attributes) {
             std::string varName = getVertexAttributeVarName(attr);
             lines += "\nin vec"
@@ -26,12 +28,20 @@ public:
                      + " "
                      + varName
                      + ";";
+
+            if (varName == "color") {
+                hasColor = true;
+            }
         }
 
         lines += "\n\nvoid main() {";
 
         // When no other instructions are provided
-        lines += "\n\tresult = vec4(color, 1.0);";
+        if (hasColor) {
+            lines += "\n\tresult = vec4(color, 1.0);";
+        } else {
+            lines += "\n\tresult = vec4(1.0, 1.0, 1.0, 1.0);";
+        }
 
         lines += "\n}";
 
--- src/opengl_msat/shader/vertex_sbldr.cpp
+++ src/opengl_msat/shader/vertex_sbldr.cpp
@@ -6,16 +6,22 @@ std::string VertexShaderBuilder::build()
     std::string lines = "#version 330 core";
 
     int loc = 0;
+    int posSize = 0;
     for (VertexAttribute attr : attributes) {
         std::string varName = getVertexAttributeVarName(attr);
+        unsigned int size = getVertexAttributeSize(attr);
         lines += "\nlayout (location = "
                 + std::to_string(loc)
                 + ") in vec"
-                + std::to_string(getVertexAttributeSize(attr))
+                + std::to_string(size)
                 + " vbo_"
                 + varName
                 + ";";
         loc++;
+
+        if (varName == "pos") {
+            posSize = size;
+        }
     }
 
     lines += "\n";
@@ -30,7 +36,13 @@ std::string VertexShaderBuilder::build()
     }
 
     lines += "\n\nvoid main() {";
-    lines += "\n\tgl_Position = vec4(vbo_pos, 1.0);";
+
+    if (posSize == 2) {
+        lines += "\n\tgl_Position = vec4(vbo_pos, 0.0, 1.0);";
+    } else {
+        lines += "\n\tgl_Position = vec4(vbo_pos, 1.0);";
+    }
+
     for (VertexAttribute attr : attributes) {
         std::string varName = getVertexAttributeVarName(attr);
         lines += "\n\t"
