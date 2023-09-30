3365ec9a6aa394db9ab1da35738d4906bd9dbbfe
markhj <hello@markhj.net>
2023-09-30 14:44:31 +0200

    Easy scene constructor

--- include/opengl_msat/shared/scenes.hpp
+++ include/opengl_msat/shared/scenes.hpp
@@ -7,12 +7,24 @@
 
 class Scene2D :
         public ObjectCollection<Object2D, VertexElement2D> {
-
+public:
+    Scene2D(std::vector<Object2D*> objects)
+    {
+        for (Object2D* obj : objects) {
+            add(obj);
+        }
+    }
 };
 
 class Scene3D :
         public ObjectCollection<Object3D, VertexElement3D> {
-
+public:
+    Scene3D(std::vector<Object3D*> objects)
+    {
+        for (Object3D* obj : objects) {
+            add(obj);
+        }
+    }
 };
 
 #endif
