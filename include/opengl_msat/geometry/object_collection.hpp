dd0881b67b24fc04d2bc9bc3d7a12f6d70f97d4c
markhj <hello@markhj.net>
2023-09-30 11:32:50 +0200

    Pass by reference

--- include/opengl_msat/geometry/object_collection.hpp
+++ include/opengl_msat/geometry/object_collection.hpp
@@ -10,8 +10,8 @@ public:
     std::vector<VertexElementXD> getVertices() override
     {
         std::vector<VertexElementXD> list;
-        for (ObjectXD obj : objects) {
-            std::vector<VertexElementXD> vs = obj.getVertices();
+        for (ObjectXD* obj : objects) {
+            std::vector<VertexElementXD> vs = obj->getVertices();
             list.insert(list.end(), vs.begin(), vs.end());
         }
         return list;
@@ -20,20 +20,20 @@ public:
     std::vector<GLfloat> getVerticesFlattened(std::vector<VertexAttribute> attributes) override
     {
         std::vector<GLfloat> list;
-        for (ObjectXD obj : objects) {
-            std::vector<GLfloat> vs = obj.getVerticesFlattened(attributes);
+        for (ObjectXD* obj : objects) {
+            std::vector<GLfloat> vs = obj->getVerticesFlattened(attributes);
             list.insert(list.end(), vs.begin(), vs.end());
         }
         return list;
     }
 
-    void add(ObjectXD obj)
+    void add(ObjectXD* obj)
     {
         objects.push_back(obj);
     }
 
 private:
-    std::vector<ObjectXD> objects;
+    std::vector<ObjectXD*> objects;
 };
 
 #endif
