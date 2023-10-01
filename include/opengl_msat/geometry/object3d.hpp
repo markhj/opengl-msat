ee3f1649fc0d8839e51239d057b30de67e27c93c
markhj <hello@markhj.net>
2023-10-01 12:53:35 +0200

    Improved Object3D

--- include/opengl_msat/geometry/object3d.hpp
+++ include/opengl_msat/geometry/object3d.hpp
@@ -9,22 +9,10 @@ class Object3D :
         public HandlesAttributes,
         public ObjectWrapper<VertexElement3D, Vec3> {
 public:
-    std::vector<VertexElement3D> getVertices() override
-    {
-        std::vector<VertexElement3D> list;
-        for (VertexElement3D v : vertices) {
-            v.position.x += translation.x;
-            v.position.y += translation.y;
-            v.position.z += translation.z;
-            list.push_back(v);
-        }
-        return list;
-    }
+    std::vector<VertexElement3D> getVertices() override;
 
-    std::vector<GLfloat> getVerticesFlattened(std::vector<VertexAttribute> attributes) override
-    {
-        return flattenVertices3D(getVertices(), attributes);
-    }
+    std::vector<GLfloat> getVerticesFlattened(std::vector<VertexAttribute> attributes) override;
+
 };
 
 #endif
--- src/opengl_msat/geometry/object3d.cpp
+++ src/opengl_msat/geometry/object3d.cpp
@@ -1,2 +1,18 @@
 #include "opengl_msat/geometry/object3d.hpp"
 
+std::vector<VertexElement3D> Object3D::getVertices()
+{
+    std::vector<VertexElement3D> list;
+    for (VertexElement3D v : vertices) {
+        v.position.x += translation.x;
+        v.position.y += translation.y;
+        v.position.z += translation.z;
+        list.push_back(v);
+    }
+    return list;
+}
+
+std::vector<GLfloat> Object3D::getVerticesFlattened(std::vector<VertexAttribute> attributes)
+{
+    return flattenVertices3D(getVertices(), attributes);
+}
