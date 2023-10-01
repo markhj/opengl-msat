9d22f2563944c0e9f6340677c8aa587c07318c7b
markhj <hello@markhj.net>
2023-10-01 10:08:33 +0200

    Initiate Cube with single argument (size)

--- include/opengl_msat/geometry/cube.hpp
+++ include/opengl_msat/geometry/cube.hpp
@@ -9,6 +9,11 @@ class Cube : public Shape3D {
 public:
     Cube(Vec3 a, Vec3 b) : a(std::move(a)), b(std::move(b)) { }
 
+    explicit Cube(const Vec3& size) :
+        a(Vec3(size.x / -2, size.y / -2, size.z / -2)),
+        b(Vec3(size.x / 2, size.y / 2, size.z / 2))
+    { }
+
     void construct() override;
 
 private:
