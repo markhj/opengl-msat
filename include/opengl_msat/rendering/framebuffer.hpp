2cf4bfbe08b4d89b1f571b431a930fa712550179
mhj-pc <5261387+markhj@users.noreply.github.com>
2023-10-22 09:48:57 +0200

    Work on fb

--- include/opengl_msat/rendering/framebuffer.hpp
+++ include/opengl_msat/rendering/framebuffer.hpp
@@ -1,7 +1,22 @@
 #ifndef OPENGL_MSAT_FRAMEBUFFER_HPP
 #define OPENGL_MSAT_FRAMEBUFFER_HPP
 
+#include "opengl_msat/common.h"
+#include "opengl_msat/textures/texture2d.hpp"
+
 class Framebuffer {
+public:
+    Framebuffer() : texture(Texture2D())
+    {
+        glGenFramebuffers(1, &fboId);
+
+        glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, texture.getTextureId(), 0);
+    }
+
+protected:
+    unsigned int fboId;
+
+    Texture2D texture;
 
 };
 
