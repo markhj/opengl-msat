1cd79bd06225ff707abcebc4709eedacd87bdea6
mhj-pc <5261387+markhj@users.noreply.github.com>
2023-11-08 17:01:26 +0100

    Rotate normals, tan and btan for 2D object

--- src/opengl_msat/geometry/object2d.cpp
+++ src/opengl_msat/geometry/object2d.cpp
@@ -2,6 +2,17 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 
+std::optional<Vec2> rotateVec2IfApplicable(std::optional<Vec2> value, float rotate)
+{
+    if (!value.has_value()) {
+        return value;
+    }
+
+    glm::mat4 rotationMatrix = glm::rotate(glm::mat4(1.0f), rotate, glm::vec3(0.0f, 0.0f, 1.0f));
+    glm::vec2 rotated = glm::vec2(rotationMatrix * glm::vec4(value.value(), 0.0f, 1.0f));
+    return Vec2(rotated.x, rotated.y);
+}
+
 std::vector <VertexElement2D> Object2D::getVertices()
 {
     std::vector<VertexElement2D> list;
@@ -21,6 +32,10 @@ std::vector <VertexElement2D> Object2D::getVertices()
 
         v.position.x += translate.x;
         v.position.y += translate.y;
+
+        v.normal = rotateVec2IfApplicable(v.normal, rotate);
+        v.tangent = rotateVec2IfApplicable(v.tangent, rotate);
+        v.bitangent = rotateVec2IfApplicable(v.bitangent, rotate);
 
         list.push_back(v);
     }
