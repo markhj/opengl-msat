f0936d2d308c6591e5a4b793efc98c4391601a95
markhj <hello@markhj.net>
2023-11-11 08:46:45 +0100

    Text/font set up + Font loading

---
+++ include/opengl_msat/text/font.hpp
@@ -0,0 +1,8 @@
+//
+// Created by pc on 07-11-2023.
+//
+
+#ifndef OPENGL_MSAT_FONT_HPP
+#define OPENGL_MSAT_FONT_HPP
+
+#endif //OPENGL_MSAT_FONT_HPP
---
+++ include/opengl_msat/text/text.hpp
@@ -0,0 +1,8 @@
+//
+// Created by pc on 07-11-2023.
+//
+
+#ifndef OPENGL_MSAT_TEXT_HPP
+#define OPENGL_MSAT_TEXT_HPP
+
+#endif //OPENGL_MSAT_TEXT_HPP
